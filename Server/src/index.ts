64972a5e1919c0e050b4e21e228be9c0e8ffa7e9
Niatsuna <niatsuna@gmail.come>
2023-05-17 00:44:44 +0200

    Init simple configuration

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,4 +1,5 @@
+import { config } from './config';
 import { Server } from "./server";
 
 export const app = new Server().app;
-export const server = app.listen(8080, '0.0.0.0');
+export const server = app.listen(config.SERVER_PORT);
