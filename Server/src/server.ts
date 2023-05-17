d44965a586461e690459956e215c7a33b5ffd6a2
Niatsuna <niatsuna@gmail.come>
2023-05-17 12:24:09 +0200

    Adjust comment for server

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -3,6 +3,12 @@ import express, { Application, Request, Response } from 'express';
 import { ApiRoutes } from './routes';
 import { Database } from './services/database.service';
 
+/**
+ * Server class
+ * 
+ * This class represents the general server written with express and
+ * initializes the database and the routes in a structured manner.
+ */
 export class Server {
     public app: Application = express();
     private db : Database = new Database();
