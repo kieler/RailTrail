1dedea258fd67ce5357caff2089391e7d8c2491a
Liam Boddin <stu222452@mail.uni-kiel.de>
2023-06-07 19:32:36 +0200

    Added some user management functionality

--- Server/src/routes/users.route.ts
+++ Server/src/routes/users.route.ts
@@ -1,79 +1,90 @@
 import { Request, Response, Router } from "express";
 import {
-  AuthenticationRequest,
-  PasswordChange,
-  UserList,
+	AuthenticationRequest,
+	PasswordChange,
+	UserList,
 } from "../models/api_types";
 
 import { authenticateJWT, jsonParser, v } from ".";
 import {
-  AuthenticationRequestSchema,
-  PasswordChangeSchema,
+	AuthenticationRequestSchema,
+	PasswordChangeSchema,
 } from "../models/jsonschemas";
+import UserService from "../services/user.service";
+import { User } from "../models";
 
 export class UsersRoute {
-  public static path: string = "/users";
-  private static instance: UsersRoute;
-  private router = Router();
+	public static path: string = "/users";
+	private static instance: UsersRoute;
+	private router = Router();
 
-  private constructor() {
-    this.router.get("", authenticateJWT, this.getUserList);
-    this.router.post("", authenticateJWT, jsonParser, this.addNewUser);
-    this.router.put(
-      "/:userId",
-      authenticateJWT,
-      jsonParser,
-      this.changePassword
-    );
-    this.router.delete("/:userId", authenticateJWT, this.deleteUser);
-  }
+	private constructor() {
+		this.router.get("", authenticateJWT, this.getUserList);
+		this.router.post("", authenticateJWT, jsonParser, this.addNewUser);
+		this.router.put(
+			"/:userId",
+			authenticateJWT,
+			jsonParser,
+			this.changePassword
+		);
+		this.router.delete("/:userId", authenticateJWT, this.deleteUser);
+	}
 
-  static get router() {
-    if (!UsersRoute.instance) {
-      UsersRoute.instance = new UsersRoute();
-    }
-    return UsersRoute.instance.router;
-  }
+	static get router() {
+		if (!UsersRoute.instance) {
+			UsersRoute.instance = new UsersRoute();
+		}
+		return UsersRoute.instance.router;
+	}
 
-  private getUserList = async (req: Request, res: Response) => {
-    // TODO: Call appropriate service method
+	private getUserList = async (req: Request, res: Response) => {
+		res.json(UserService.getAllUsers());
+		return;
+	};
 
-    // This should be deleted later on:
-    const ret: UserList = { users: [{ id: 1, username: "hallo welt" }] };
-    res.json(ret);
-    return;
-  };
+	private addNewUser = async (req: Request, res: Response) => {
+		const userData: AuthenticationRequest = req.body;
+		if (!userData || v.validate(userData, AuthenticationRequestSchema).valid) {
+			res.sendStatus(400);
+			return;
+		}
+		const ret: Promise<User | null> = UserService.createUser(userData.username, userData.password);
+		if (ret == null) {
+			res.sendStatus(500);
+		}
 
-  private addNewUser = async (req: Request, res: Response) => {
-    const userData: AuthenticationRequest = req.body;
-    if (!userData || v.validate(userData, AuthenticationRequestSchema).valid) {
-      res.sendStatus(400);
-      return;
-    }
-    // TODO: Call appropriate service method
+		res.sendStatus(200);
+		return;
+	};
 
-    res.sendStatus(200);
-    return;
-  };
+	private changePassword = async (req: Request, res: Response) => {
+		const userData: PasswordChange = req.body;
+		if (!userData || v.validate(userData, PasswordChangeSchema).valid) {
+			res.sendStatus(400);
+			return;
+		}
+		const success: boolean = await UserService.updatePassword(req.params.username, userData);
+		if (!success){
+			res.sendStatus(400);
+			return
+		}
 
-  private changePassword = async (req: Request, res: Response) => {
-    const userData: PasswordChange = req.body;
-    if (!userData || v.validate(userData, PasswordChangeSchema).valid) {
-      res.sendStatus(400);
-      return;
-    }
-    // TODO: Call appropriate service method
+		res.sendStatus(200);
+		return;
+	};
 
-    res.sendStatus(200);
-    return;
-  };
-
-  private deleteUser = async (req: Request, res: Response) => {
-    if (!req.params || !req.params.userId) {
-      res.sendStatus(400);
-      return;
-    }
-    const userIdToBeDeleted: number = parseInt(req.params?.userId);
-    // TODO: Call appropriate service method
-  };
+	private deleteUser = async (req: Request, res: Response) => {
+		if (!req.params || !req.params.userId) {
+			res.sendStatus(400);
+			return;
+		}
+		const userIdToBeDeleted: number = parseInt(req.params?.userId);
+		const successful: boolean = await UserService.removeUser(userIdToBeDeleted, req.params.username);
+		if (successful) {
+			res.sendStatus(200)
+			return
+		}
+		res.sendStatus(400)
+		return
+	};
 }
--- Server/src/services/user.service.ts
+++ Server/src/services/user.service.ts
@@ -1,34 +1,55 @@
-import { User } from "../models"
-import { logger } from "../utils/logger"
-import { Database } from "./database.service"
-import UserController from "./db/user.controller"
+import { User } from "../models";
+import { PasswordChange } from "../models/api_types";
+import { logger } from "../utils/logger";
+import { CryptoService } from "./crypto.service";
+import { Database } from "./database.service";
+import UserController from "./db/user.controller";
 
 /**
  * Service for user management
  */
-export default class UserService{
+export default class UserService {
+    private static controller: UserController = new Database().users;
 
-    private static controller: UserController = new Database().users
-
+    private static cryptoservice: CryptoService = new CryptoService();
     /**
      * Create a new user
      * @param name (unique) username of the new user
      * @param password password of the new user
      * @returns created `User` if successful, `null` otherwise
      */
-    public static async createUser(name: string, password: string):Promise<User | null>{
-        // TODO: implement, this is only generic
-        return this.controller.save(name, password)
+    public static async createUser(
+        name: string,
+        password: string
+    ): Promise<User | null> {
+        const user: User | null = this.controller.getByUsername(name)
+        if (!user) {
+            logger.info(`User with username ${name} already exists`)
+            return null
+        }
+        // Might add something such that this is only possible if no user is registered yet
+
+        logger.info("Hashing password!");
+        const hashed_pass: string | undefined = await this.cryptoservice.produceHash(
+            password
+        );
+
+        if (hashed_pass) {
+            // TODO: Check if this works when real implementation is there.
+            const addedUser: User = this.controller.save(name, hashed_pass)
+        }
+        logger.info(`User ${name} was successfully added`)
+        return user
     }
 
     /**
-     * 
+     *
      * @param id id of the user
      * @returns `User` with id `id` or `null` if no user with `id` exists
      */
-    public static async getUserById(id: number): Promise<User | null>{
+    public static async getUserById(id: number): Promise<User | null> {
         // TODO: implement, this is only generic
-        return this.controller.getById(id)
+        return this.controller.getById(id);
     }
 
     /**
@@ -36,41 +57,90 @@ export default class UserService{
      * @param name username of the user
      * @returns `User` with username `name` if it exists, `null` otherwise
      */
-    public static async getUserByName(name: string): Promise<User | null>{
+    public static async getUserByName(name: string): Promise<User | null> {
         // TODO: implement, this is only generic
-        return this.controller.getByUsername(name)
+        return this.controller.getByUsername(name);
     }
 
     /**
-     * 
+     *
      * @param user `User` which gets a new name
      * @param name new username
      * @returns `User` if successful, `null` otherwise
      */
-    public static async setUserName(user: User, name: string): Promise<User | null>{
+    public static async setUserName(
+        user: User,
+        name: string
+    ): Promise<User | null> {
         // TODO: implement
-        return null
+        return null;
     }
 
     /**
-     * 
+     *
      * @param user `User` which gets a new password
-     * @param password new password
+     * @param password new hashed password
      * @returns `User` if successful, `null` otherwise
      */
-    public static async setUserPassword(user: User, password: string): Promise<User | null>{
-        // TODO: implement
-        return null
+    public static async setUserPassword(
+        user: User,
+        password: string
+    ): Promise<User | null> {
+        user.password = password
+        return user;
     }
 
     /**
-     * Delete a user
+     * Updates the password of a given user.
+     * @param username The username of the user that wants to change their password
+     * @param passwordChange The information containing the old and the new plain passwords
+     * @returns `true`, if the password was successfully updated, `false` otherwise
+     */
+    public static async updatePassword(username : string, passwordChange: PasswordChange): Promise<boolean> {
+        const user: User | null = await this.getUserByName(username);
+        if (!user) {
+            return false;
+        }
+        if (!await this.cryptoservice.verify(user.password, passwordChange.oldPassword)) {
+            logger.err("The old password is not correct")
+            return false;
+        }
+        const hashedPassword: string | undefined = await this.cryptoservice.produceHash(passwordChange.newPassword)
+        if (!hashedPassword) {
+            logger.err("Hashing of password was not successful")
+            return false;
+        }
+        const successfulUpdate: User | null = await this.setUserPassword(user, hashedPassword)
+
+        return successfulUpdate != null;
+    }
+
+    /**
+     * Delete a user.
      * @param user `User` to delete
      * @returns `true` if deletion was successful, `false` otherwise
      */
-    public static async removeUser(user: User): Promise<boolean>{
-        // TODO: implement, this is only generic
-        this.controller.remove(user)
-        return false
+    public static async removeUser(id:number, currentUsername : string): Promise<boolean> {
+        const currentUser: User | null = await this.getUserByName(currentUsername)
+        if (!currentUser) {
+            logger.err(`Could not find current user with username ${currentUsername}.`)
+            return false
+        }     
+        const userToBeDeleted: User | null = await this.getUserById(id)
+        if (!userToBeDeleted) {
+            logger.err(`Could not find the user to be deleted with id ${id}`)
+            return false
+        }   
+        this.controller.remove(userToBeDeleted)
+        return true;
+    }
+
+    /**
+     * Get the full list of users.
+     * @returns A full `User` list if successful, `null` otherwise.
+     */
+    public static async getAllUsers(): Promise<User[] | null> {
+        // TODO: Wait for db to implement this.
+        return null;
     }
 }
