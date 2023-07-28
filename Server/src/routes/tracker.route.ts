263c3e7f2586a4e0a6f9ff2b82fffd71b5fba731
Nico Biernat <nico.biernat@gmail.com>
2023-07-28 13:35:24 +0200

    Fix tracker route and add jsonschema

--- Server/src/routes/tracker.route.ts
+++ Server/src/routes/tracker.route.ts
@@ -1,7 +1,8 @@
 import { Request, Response, Router } from "express";
 import { logger } from "../utils/logger";
-import { authenticateJWT, jsonParser, v } from ".";
+import { authenticateJWT, jsonParser, v, validateSchema } from ".";
 import TrackerService from "../services/tracker.service"
+import { UplinkSchemaTracker } from "../models/jsonschemas.tracker";
 
 
 export class TrackerRoute {
@@ -21,32 +22,31 @@ export class TrackerRoute {
     }
 
     private oysterLorawan = async (req: Request, res: Response) => {
+        // TODO: generate TypeScript interfaces from jsonschema
         const trackerData = req.body;
-        if (!trackerData) {
-            res.sendStatus(400);
-            return;
-        }
-
-        if(trackerData.uplink_message.decoded_payload.fixFailed == "true") {
-            logger.info("Fix failed for tracker ${trackerData.end_device_ids.device_id}");
-
-        } else {
-            let trackerId = trackerData.end_device_ids.device_id;
-            let timestamp = new Date(trackerData.received_at);
-            let position = JSON.parse(JSON.stringify([trackerData.uplink_message.decoded_payload.longitudeDeg, trackerData.uplink_message.decoded_payload.latitudeDeg]));
-            let heading = trackerData.uplink_message.decoded_payload.headingDeg;
-            let speed = trackerData.uplink_message.decoded_payload.speedKmph;
-            let battery = trackerData.uplink_message.decoded_payload.batV;
-            switch (trackerData.uplink_message.f_port) {
-                case 1:
-                    TrackerService.appendLog(trackerId, timestamp, position, heading, speed, battery, trackerData);
-                    break;
-                case 30:
+        if (!validateSchema(trackerData, UplinkSchemaTracker)) {
+			res.sendStatus(400)
+			return
+		}
+        
+        let trackerId = trackerData.end_device_ids.device_id;
+        switch (trackerData.uplink_message?.f_port) {
+            case 1:
+                if (trackerData.uplink_message.decoded_payload.fixFailed) {
+                    logger.info("Fix failed for tracker ${trackerData.end_device_ids.device_id}");
                     TrackerService.registerTracker(trackerId, trackerData);
                     break;
-                default:
-                    break;
-            }
+                }
+                let timestamp = new Date(trackerData.received_at);
+                let position = JSON.parse(JSON.stringify([trackerData.uplink_message.decoded_payload.longitudeDeg, trackerData.uplink_message?.decoded_payload?.latitudeDeg]));
+                let heading = trackerData.uplink_message.decoded_payload.headingDeg;
+                let speed = trackerData.uplink_message.decoded_payload.speedKmph;
+                let battery = trackerData.uplink_message.decoded_payload.batV;
+                TrackerService.appendLog(trackerId, timestamp, position, heading, speed, battery, trackerData);
+                break;
+            case 30:
+                TrackerService.registerTracker(trackerId, trackerData);
+                break;
         }
         res.sendStatus(200);
         return;
