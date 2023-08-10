04e64e0dd3a3be83123066e3e15e76cc50982be4
Liam Boddin <stu222452@mail.uni-kiel.de>
2023-08-10 14:55:59 +0200

    Refactored login service

--- Server/src/services/login.service.ts
+++ Server/src/services/login.service.ts
@@ -1,94 +1,74 @@
-import {
-  AuthenticationRequest,
-  AuthenticationResponse,
-} from "../models/api.website"
-import { User } from "../models/user"
-import { logger } from "../utils/logger"
+import {AuthenticationRequest, AuthenticationResponse,} from "../models/api.website"
+import {User} from "../models"
+import {logger} from "../utils/logger"
 import * as jwt from "jsonwebtoken"
 import * as argon from "argon2"
 
-import { accessTokenSecret } from "../routes"
+import {accessTokenSecret} from "../routes"
 import database from "./database.service"
+import CryptoService from "./crypto.service";
 
 /**
  * A class that manages the users.
  */
 export default class LoginService {
 
-  /**
-   * Produces a hash using the argon hashing.
-   * @param input The password, that needs to be hashed
-   * @returns Undefined, if the hashing is unsuccessful, a hash of the password otherwise.
-   */
-  private async produceHash(input: string): Promise<string | undefined> {
-    try {
-      return argon.hash(input)
-    } catch (err) {
-      return
-    }
-  }
+    /**
+     * Login process for a user that is already in the database.
+     * @param auth The authentication details.
+     * @returns A jsonwebtoken if login successful, undefined otherwise.
+     */
+    public async login(
+        auth: AuthenticationRequest
+    ): Promise<AuthenticationResponse | undefined> {
+        const user = await database.users.getByUsername(auth.username)
+        if (!user) {
+            return
+        }
 
-  /**
-   * Login process for a user that is already in the database.
-   * @param auth The authentication details.
-   * @returns A jsonwebtoken if login successful, undefined otherwise.
-   */
-  public async login(
-    auth: AuthenticationRequest
-  ): Promise<AuthenticationResponse | undefined> {
-    const user = await database.users.getByUsername(auth.username)
-    if (user != null) {
-      const password = user.password
-      let isCorrectPassword: boolean
-      try {
-        isCorrectPassword = await argon.verify(password, auth.password)
-      } catch (err) {
-        isCorrectPassword = false
-      }
-      if (isCorrectPassword) {
-        // TODO: Could put expires in. That needs a refresh token possibility.
-        const accessToken = jwt.sign(
-          { username: user.username },
-          accessTokenSecret
-        )
+        const password: string = user.password
+        try {
+            await argon.verify(password, auth.password)
+        } catch (err) {
+            return
+        }
+
+        const accessToken = jwt.sign({username: user.username}, accessTokenSecret)
         logger.info(`User ${user.username} successfully logged in`)
-        return { token: accessToken }
-      }
+        return {token: accessToken}
     }
-    return
-  }
 
-  /**
-   * Sign up process. This should only be possible for testing and adding users.
-   * @param auth The authentication information from the request
-   * @returns An AuthenticationResponse with a session token or undefined, if something went wrong.
-   */
-  public async signup(
-    auth: AuthenticationRequest
-  ): Promise<AuthenticationResponse | undefined> {
-    // TODO: Check if works when real implementation is there.
-    const user: User | null = await database.users.getByUsername(auth?.username)
-    // Might add something such that this is only possible if no user is registered yet
+    /**
+     * Sign up process. This should only be possible for testing and adding users.
+     * @param auth The authentication information from the request
+     * @returns An AuthenticationResponse with a session token or undefined, if something went wrong.
+     */
+    public async signup(
+        auth: AuthenticationRequest
+    ): Promise<AuthenticationResponse | undefined> {
+        const user: User | null = await database.users.getByUsername(auth?.username)
+        // Might add something such that this is only possible if no user is registered yet
 
-    if (!user && auth.username && auth.password) {
-      logger.info("Hashing password!")
-      const hashed_pass: string | undefined = await this.produceHash(
-        auth.password
-      )
-      if (hashed_pass) {
-        // TODO: Check if this works when real implementation is there.
-        await database.users.save(
-          auth.username,
-          hashed_pass
+        if (!user || !auth.username || !auth.password) {
+            return
+        }
+        logger.info("Hashing password!")
+        const hashed_pass: string | undefined = await CryptoService.produceHash(
+            auth.password
         )
-        const accessToken = jwt.sign(
-          { username: auth.username },
-          accessTokenSecret
+        if (!hashed_pass) {
+            return
+        }
+        const createdUser: User | null = await database.users.save(auth.username, hashed_pass)
+        if (!createdUser) {
+            return
+        }
+
+        const accessToken: string = jwt.sign(
+            {username: auth.username},
+            accessTokenSecret
         )
         logger.info(`User ${auth.username} successfully signed in`)
-        return { token: accessToken }
-      }
+        return {token: accessToken}
     }
-    return undefined
-  }
 }
