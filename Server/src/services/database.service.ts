7c2c16b744948bfdf136a4d8eba9d24e1687a249
Niatsuna <niatsuna@gmail.com>
2023-08-15 17:35:44 +0200

    Remove unnecessary import

--- Server/src/services/database.service.ts
+++ Server/src/services/database.service.ts
@@ -1,4 +1,3 @@
-import { config } from '../config';
 import { PrismaClient } from '@prisma/client'
 import UserController from './db/user.controller';
 import POIController from './db/poi.controller';
