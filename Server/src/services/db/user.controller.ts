f0d3f2bf9601a9a5c1eb62a2862e2dac68fdda53
Niatsuna <niatsuna@gmail.com>
2023-08-15 18:45:59 +0200

    Adjust UserController

--- Server/src/services/db/user.controller.ts
+++ Server/src/services/db/user.controller.ts
@@ -1,6 +1,5 @@
 import { PrismaClient, Prisma } from '@prisma/client';
 import type { User } from '@prisma/client';
-import { logger } from '../../utils/logger';
 
 /**
  * UserController class
@@ -22,67 +21,49 @@ export default class UserController {
     /**
      * Saves an user in the database.
      *
+     * The parameter are given via object deconstruction from the model `User`!
+     * Currently given parameters are:
      * @param username - **unique** name of the user.
      * @param password - **hashed** password.
-     * @returns User | null if an error occurs.
+     * @returns User
      */
-    public async save(username: string, password: string): Promise<User | null> {
-        try {
-            return await this.prisma.user.create({
-                data: {
-                    username: username,
-                    password: password
-                }
-            });
-        } catch (e) {
-            logger.debug(e)
-            return null
-        }
+    public async save(args: Prisma.UserCreateInput): Promise<User> {
+        return await this.prisma.user.create({
+            data: args
+        });
     }
 
     /**
      * Updates an user in the database.
      *
      * @param name - Old username before change. Indicator which user should be updated
+     *
+     * The parameter are given via object deconstruction from the model `User`!
+     * Currently given parameters are:
      * @param username - New username after change. (Optional)
      * @param password - New password after change. (Optional)
-     * @returns User | null if an error occurs.
+     * @returns User
      */
-    public async update(name: string, username?: string, password?: string): Promise<User | null> {
-        try {
-            return await this.prisma.user.update({
-                where: {
-                    username: name
-                },
-                data: {
-                    username: username,
-                    password: password
-                }
-            });
-        } catch (e) {
-            logger.debug(e)
-            return null
-        }
+    public async update(name: string, args: Prisma.UserUpdateInput): Promise<User> {
+        return await this.prisma.user.update({
+            where: {
+                username: name
+            },
+            data: args
+        });
     }
 
     /**
      * Removes an user from the database.
      *
      * @param username - Indicator which user should be removed
-     * @returns True | False depending on if the user was removed or not.
      */
-    public async remove(username: string): Promise<Boolean> {
-        try {
-            await this.prisma.user.delete({
-                where: {
-                    username: username
-                }
-            });
-            return true
-        } catch (e) {
-            logger.debug(e)
-            return false
-        }
+    public async remove(username: string): Promise<void> {
+        await this.prisma.user.delete({
+            where: {
+                username: username
+            }
+        });
     }
 
     /**
@@ -91,12 +72,7 @@ export default class UserController {
      * @returns `User[]` - List of all users.
      */
     public async getAll(): Promise<User[]> {
-        try {
-            return await this.prisma.user.findMany({});
-        } catch (e) {
-            logger.debug(e)
-            return []
-        }
+        return await this.prisma.user.findMany({});
     }
 
     /**
@@ -106,15 +82,10 @@ export default class UserController {
      * @returns User | null depending on if the user could be found.
      */
     public async getByUsername(username: string): Promise<User | null> {
-        try {
-            return await this.prisma.user.findUnique({
-                where: {
-                    username: username
-                }
-            });
-        } catch (e) {
-            logger.debug(e)
-            return null
-        }
+        return await this.prisma.user.findUnique({
+            where: {
+                username: username
+            }
+        });
     }
 }
