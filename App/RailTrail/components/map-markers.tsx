e890c708c533c0410aaed64a852e30eba513950b
Jannis Gehrt <jannis.gehrt@cap3.de>
2023-09-01 11:42:16 +0200

    Change z index of markers

--- App/RailTrail/components/map-markers.tsx
+++ App/RailTrail/components/map-markers.tsx
@@ -11,7 +11,6 @@ import TrainBackgroundHeading from "../assets/icons/train-background-heading"
 import TrainBackgroundNeutral from "../assets/icons/train-background-neutral"
 import { Position } from "../types/position"
 import PassingPosition from "../assets/icons/passing-position"
-import { View } from "react-native"
 
 interface ExternalProps {
   readonly location: Location.LocationObject | null
@@ -35,118 +34,120 @@ export const MapMarkers = ({
   track,
   useSmallMarker,
   mapHeading,
-}: Props) => (
-  <>
-    {calculatedPosition ? (
-      <Marker
-        key={0}
-        zIndex={Number.MAX_VALUE}
-        anchor={{ x: 0.5, y: 0.5 }}
-        coordinate={{
-          latitude: calculatedPosition.lat,
-          longitude: calculatedPosition.lng,
-        }}
-      >
-        <UserLocation />
-      </Marker>
-    ) : location ? (
-      <Marker
-        key={0}
-        zIndex={Number.MAX_VALUE}
-        anchor={{ x: 0.5, y: 0.5 }}
-        coordinate={{
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
-        }}
-      >
-        <UserLocation />
-      </Marker>
-    ) : null}
-    {pointsOfInterest.map((poi, index) => {
-      return (
+}: Props) => {
+  return (
+    <>
+      {calculatedPosition ? (
         <Marker
-          key={index}
+          key={0}
+          zIndex={100000}
           anchor={{ x: 0.5, y: 0.5 }}
           coordinate={{
-            latitude: poi.pos.lat,
-            longitude: poi.pos.lng,
+            latitude: calculatedPosition.lat,
+            longitude: calculatedPosition.lng,
           }}
         >
-          <PointOfInterestMarker
-            pointOfInterestType={poi.typeId}
-            useSmallMarker={useSmallMarker}
-          />
+          <UserLocation />
         </Marker>
-      )
-    })}
-    {vehicles.map((vehicle) => {
-      return (
+      ) : location ? (
         <Marker
-          key={"foreground" + vehicle.id}
+          key={0}
+          zIndex={100000}
           anchor={{ x: 0.5, y: 0.5 }}
           coordinate={{
-            latitude: vehicle.pos.lat,
-            longitude: vehicle.pos.lng,
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
           }}
-          zIndex={11 + vehicle.id}
         >
-          {useSmallMarker ? (
-            <TrainForeground width={15} height={18} />
-          ) : (
-            <TrainForeground />
-          )}
+          <UserLocation />
         </Marker>
-      )
-    })}
-    {vehicles.map((vehicle) => {
-      return (
-        <Marker
-          key={"background" + vehicle.id}
-          anchor={{ x: 0.5, y: 0.5 }}
-          coordinate={{
-            latitude: vehicle.pos.lat,
-            longitude: vehicle.pos.lng,
-          }}
-          rotation={
-            vehicle.heading != undefined
-              ? vehicle.heading - mapHeading
-              : undefined
-          }
-          zIndex={10 + vehicle.id}
-        >
-          {useSmallMarker ? (
-            vehicle.heading != null ? (
-              <TrainBackgroundHeading width={32} height={32} />
+      ) : null}
+      {pointsOfInterest.map((poi, index) => {
+        return (
+          <Marker
+            key={index}
+            anchor={{ x: 0.5, y: 0.5 }}
+            coordinate={{
+              latitude: poi.pos.lat,
+              longitude: poi.pos.lng,
+            }}
+          >
+            <PointOfInterestMarker
+              pointOfInterestType={poi.typeId}
+              useSmallMarker={useSmallMarker}
+            />
+          </Marker>
+        )
+      })}
+      {vehicles.map((vehicle, index) => {
+        return (
+          <Marker
+            key={"foreground" + vehicle.id}
+            anchor={{ x: 0.5, y: 0.5 }}
+            coordinate={{
+              latitude: vehicle.pos.lat,
+              longitude: vehicle.pos.lng,
+            }}
+            zIndex={11 + index}
+          >
+            {useSmallMarker ? (
+              <TrainForeground width={15} height={18} />
             ) : (
-              <TrainBackgroundNeutral width={32} height={32} />
-            )
-          ) : vehicle.heading != null ? (
-            <TrainBackgroundHeading />
+              <TrainForeground />
+            )}
+          </Marker>
+        )
+      })}
+      {vehicles.map((vehicle, index) => {
+        return (
+          <Marker
+            key={"background" + vehicle.id}
+            anchor={{ x: 0.5, y: 0.5 }}
+            coordinate={{
+              latitude: vehicle.pos.lat,
+              longitude: vehicle.pos.lng,
+            }}
+            rotation={
+              vehicle.heading != undefined
+                ? vehicle.heading - mapHeading
+                : undefined
+            }
+            zIndex={10 + index}
+          >
+            {useSmallMarker ? (
+              vehicle.heading != null ? (
+                <TrainBackgroundHeading width={32} height={32} />
+              ) : (
+                <TrainBackgroundNeutral width={32} height={32} />
+              )
+            ) : vehicle.heading != null ? (
+              <TrainBackgroundHeading />
+            ) : (
+              <TrainBackgroundNeutral />
+            )}
+          </Marker>
+        )
+      })}
+      {passingPosition ? (
+        <Marker
+          key={1}
+          zIndex={10}
+          anchor={{ x: 0.5, y: 0.5 }}
+          coordinate={{
+            latitude: passingPosition.lat,
+            longitude: passingPosition.lng,
+          }}
+        >
+          {useSmallMarker ? (
+            <PassingPosition width={32} height={32} />
           ) : (
-            <TrainBackgroundNeutral />
+            <PassingPosition />
           )}
         </Marker>
-      )
-    })}
-    {passingPosition ? (
-      <Marker
-        key={1}
-        zIndex={10}
-        anchor={{ x: 0.5, y: 0.5 }}
-        coordinate={{
-          latitude: passingPosition.lat,
-          longitude: passingPosition.lng,
-        }}
-      >
-        {useSmallMarker ? (
-          <PassingPosition width={32} height={32} />
-        ) : (
-          <PassingPosition />
-        )}
-      </Marker>
-    ) : null}
-    {track ? (
-      <Geojson geojson={track} strokeColor={Color.track} strokeWidth={6} />
-    ) : null}
-  </>
-)
+      ) : null}
+      {track ? (
+        <Geojson geojson={track} strokeColor={Color.track} strokeWidth={6} />
+      ) : null}
+    </>
+  )
+}
