82f8c59d77389a7c69e4e7e7b18af06a97e55d57
Liam Boddin <stu222452@mail.uni-kiel.de>
2023-09-07 16:34:18 +0200

    improved error handling

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -14,8 +14,8 @@ export class Server {
 	public app: Application = express()
 
 	constructor() {
-		this.app.use(mapPrismaErrorToHttpCodes)
 		this.app.use(morganMiddleware) //request logging
 		this.app.use(ApiRoutes.path, ApiRoutes.router)
+		this.app.use(mapPrismaErrorToHttpCodes)
 	}
 }
