bf8a72b81083a28292c2470cb0301ceadbc590ac
Liam Boddin <stu222452@mail.uni-kiel.de>
2023-09-14 11:16:30 +0200

    Removed unnecessary call for trackers in app vehicle route

--- Server/src/routes/vehicle.route.ts
+++ Server/src/routes/vehicle.route.ts
@@ -127,7 +127,6 @@ export class VehicleRoute {
 					const heading = await VehicleService.getVehicleHeading(v)
 					const speed = await VehicleService.getVehicleSpeed(v)
 					const pos = await VehicleService.getVehiclePosition(v, heading, speed)
-					const trackers: Tracker[] = await database.trackers.getByVehicleId(v.uid)
 					const nearbyVehicleTrackKm: number | null = pos ? GeoJSONUtils.getTrackKm(pos) : null
 					if (nearbyVehicleTrackKm == null) {
 						logger.error(`Could not compute track kilometer for vehicle with id ${v.uid}
@@ -137,7 +136,7 @@ export class VehicleRoute {
 							name: v.name,
 							track: v.trackId,
 							type: v.typeId,
-							trackerIds: trackers.map(t => t.uid),
+							trackerIds: [],  // The app doesn't care about the trackerIds
 							pos: pos ? { lat: GeoJSONUtils.getLatitude(pos), lng: GeoJSONUtils.getLongitude(pos) } : undefined,
 							percentagePosition: -1,
 							heading: heading,
@@ -154,7 +153,7 @@ export class VehicleRoute {
 						name: v.name,
 						track: v.trackId,
 						type: v.typeId,
-						trackerIds: trackers.map(t => t.uid),
+						trackerIds: [], // The app doesn't care about the trackerIds
 						pos: pos ? { lat: GeoJSONUtils.getLatitude(pos), lng: GeoJSONUtils.getLongitude(pos) } : undefined,
 						percentagePosition: (await TrackService.getTrackKmAsPercentage(nearbyVehicleTrackKm, track)) ?? -1,
 						heading: heading,
