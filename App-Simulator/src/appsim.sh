cc6a37a555e187f8b6755994691ae6177aa5a52f
Liam Boddin <stu222452@mail.uni-kiel.de>
2023-09-14 12:25:52 +0200

    added shell script by Nico

---
+++ App-Simulator/src/appsim.sh
@@ -0,0 +1,10 @@
+# Arg1: number of concurrent requests, Arg2: sleep time between request bursts in seconds
+while true; do
+    for i in $(seq 1 $1); do
+    {
+        curl -X PUT http://localhost:8080/api/vehicles/app --json '{"vehicleId":1, "pos":{"lat": 54.279806, "lng": 10.611638}, "speed":15, "heading":1}';
+        printf " - Response from request: ${i}\n";
+    } &
+    done;
+    sleep $2;
+done
