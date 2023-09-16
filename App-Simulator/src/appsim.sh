59b6f5ec99f926d797608a8fdb451d8a5d346221
Liam Boddin <stu222452@mail.uni-kiel.de>
2023-09-16 10:21:50 +0200

    Updated app simulator for timestamp

--- App-Simulator/src/appsim.sh
+++ App-Simulator/src/appsim.sh
@@ -2,8 +2,7 @@
 while true; do
     for i in $(seq 1 $1); do
     {
-        curl -X PUT http://localhost:8080/api/vehicles/app --json '{"vehicleId":1, "pos":{"lat": 54.279806, "lng": 10.611638}, "speed":15, "heading":1}';
-        printf " - Response from request: ${i}\n";
+curl -X PUT http://localhost:8080/api/vehicles/app --json '{"vehicleId":1, "pos": {"lat": 54.279806, "lng": 10.611638}, "speed":15, "heading":1, "timestamp": ' --json "$(date +%s%3N)" --json ' }';        printf " - Response from request: ${i}\n";
     } &
     done;
     sleep $2;
